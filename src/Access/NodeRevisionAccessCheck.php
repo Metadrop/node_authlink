aecb9832b8f42e2eaa54407fe5b71d57d20c5ed4
David López <david.lopez@metadrop.net>
2018-10-22 14:48:31 +0200

    SP-832199932200423: Add support to node_authlink for revisions.

---
+++ src/Access/NodeRevisionAccessCheck.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\node_authlink\Access;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\node\Access\NodeRevisionAccessCheck as NodeRevisionAccessCheckOriginal;
+use Drupal\node\NodeInterface;
+
+/**
+ * Class NodeRevisionAccessCheck
+ *
+ * @package Drupal\node_authlink\Access
+ */
+class NodeRevisionAccessCheck extends NodeRevisionAccessCheckOriginal {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function checkAccess(NodeInterface $node, AccountInterface $account, $op = 'view') {
+    if (node_authlink_node_is_enabled($node) && node_authlink_check_authlink($node, $op, $account)) {
+      return AccessResult::allowed();
+    }
+
+    parent::checkAccess($node, $account, $op);
+  }
+
+}
